8635a9ca94b8491a8052c4faa005641c628d87af
rohit_reddy <rohithreddyedulla@gmail.com>
2024-04-10 11:44:38 +0530

    deployment change

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -2,7 +2,7 @@ import { ID, Query } from "appwrite";
 
 import { appwriteConfig, account, databases, storage, avatars } from "./config";
 import { IUpdatePost, INewPost, INewUser, IUpdateUser } from "@/types";
-import { log } from "console";
+
 
 // ============================================================
 // AUTH
